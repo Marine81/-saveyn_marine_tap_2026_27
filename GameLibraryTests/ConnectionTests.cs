b6db5fd777f1392da2c96f4ec4156456d3d3e5f7
Marine81 <marinesaveyn14@gmail.com>
2026-09-25 12:05:49 +0200

    test connection
    ajout methode createlocation et createmessage et test

---
+++ GameLibraryTests/ConnectionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using activity_00_tap_26_27.Core.Components;
+using activity_00_tap_26_27.Core;
+
+namespace GameLibraryTests
+{
+    public class ConnectionTests
+    {
+        private LocationComponent CreateLocation(string name)
+        {
+            GameObject gameObject = new GameObject(name);
+            LocationComponent locationComponent = new LocationComponent(name);
+
+            locationComponent.SetOwner(gameObject);//liaison
+            gameObject.AddComponent(locationComponent);//ajout du component
+
+            return locationComponent;
+        }
+
+        [Test]
+        public void GetDestination_ReturnsConstructionDestination()
+        {
+            LocationComponent destination = CreateLocation("Daisy Town");
+            Connection connection = new Connection(destination, 3);
+
+            Assert.That(connection.GetDestination(), Is.EqualTo(destination));
+            Assert.That(connection.GetDistance(), Is.EqualTo(3.0f).Within(0.001f));
+        }
+    }
+}
--- GameLibraryTests/obj/Debug/net8.0/GameLibraryTests.AssemblyInfo.cs
+++ GameLibraryTests/obj/Debug/net8.0/GameLibraryTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameLibraryTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1947fa25deafac6880458ed8fb8db1e468e39e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e4ba95fef0924e6fed27ab3c8770fecb946879")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameLibraryTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameLibraryTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
